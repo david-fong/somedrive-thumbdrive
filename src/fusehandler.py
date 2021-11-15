df496baed9ba52d7518688a2a9e9d4aff6a4d74e
david-fong <davidfong19@gmail.com>
2021-11-15 13:42:52 -0800

    add stub files

---
+++ src/fusehandler.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+# from __future__ import print_function, absolute_import, division
+
+import logging
+import os
+
+from errno import EACCES
+from os.path import realpath
+from threading import Lock
+
+import fuse
+
+# Note: The ChaCha20 stream cipher seems like a sensible choice
+# provides efficient random access and has some strengths over AES.
+
+
+def is_drive_encrypted(root: str) -> bool:
+    pass # TODO
+
+
+def fully_encrypt_drive(root: str, key: bytearray):
+    # TODO
+    # throw if the brand file is present
+    # add the brand file. it contains a hash of the key
+    #   Interesting: https://stackoverflow.com/questions/25432139/python-cross-platform-hidden-file
+    # walk root and encrypt. skip links.
+    pass
+
+
+def fully_decrypt_drive(root: str, key: bytearray):
+    # TODO
+    # throw if brand_file is not present
+    # check that the key hashes to the value in the brand file
+    # remove the brand file
+    # walk root and decrypt. skip links.
+    pass
+
+
+class CrytoOperations(fuse.LoggingMixIn, fuse.Operations):
+    """
+    https://libfuse.github.io/doxygen/structfuse__operations.html
+    """
+    def __init__(self, root, key: bytearray):
+        # TODO throw if brand file is missing or key does not hash to its contents
+        self.root = realpath(root)
+        self.key = key
+        self.rwlock = Lock()
+
+    def __call__(self, op, path, *args):
+        return super(CrytoOperations, self).__call__(op, self.root + path, *args)
+
+    def access(self, path, mode):
+        if not os.access(path, mode):
+            raise fuse.FuseOSError(EACCES)
+
+    chmod = os.chmod
+    chown = os.chown
+
+    def create(self, path, mode):
+        return os.open(path, os.O_WRONLY | os.O_CREAT | os.O_TRUNC, mode)
+
+    def flush(self, path, fh):
+        return os.fsync(fh)
+
+    def fsync(self, path, datasync, fh):
+        if datasync != 0:
+            return os.fdatasync(fh)
+        else:
+            return os.fsync(fh)
+
+    def getattr(self, path, fh=None):
+        st = os.lstat(path)
+        return dict((key, getattr(st, key)) for key in (
+            'st_atime', 'st_ctime', 'st_gid', 'st_mode', 'st_mtime',
+            'st_nlink', 'st_size', 'st_uid', 'std_blocks'))
+
+    getxattr = None
+
+    def link(self, target, source):
+        return os.link(self.root + source, target)
+
+    listxattr = None
+    mkdir = os.mkdir
+    mknod = os.mknod
+    open = os.open
+
+    def read(self, path, size, offset, fh):
+        with self.rwlock:
+            os.lseek(fh, offset, 0)
+            # TODO decrypt data
+            return os.read(fh, size)
+
+    def readdir(self, path, fh):
+        return ['.', '..'] + os.listdir(path)
+
+    readlink = os.readlink
+
+    def release(self, path, fh):
+        return os.close(fh)
+
+    def rename(self, old, new):
+        return os.rename(old, self.root + new)
+
+    rmdir = os.rmdir
+
+    def statfs(self, path):
+        stv = os.statvfs(path)
+        return dict((key, getattr(stv, key)) for key in (
+            'f_bavail', 'f_bfree', 'f_blocks', 'f_bsize', 'f_favail',
+            'f_ffree', 'f_files', 'f_flag', 'f_frsize', 'f_namemax'))
+
+    def symlink(self, target, source):
+        return os.symlink(source, target)
+
+    def truncate(self, path, length, fh=None):
+        with open(path, 'r+') as f:
+            f.truncate(length)
+
+    unlink = os.unlink
+    utimens = os.utime
+
+    def write(self, path, data, offset, fh):
+        # TODO encrypt data
+        with self.rwlock:
+            os.lseek(fh, offset, 0)
+            return os.write(fh, data)
+
+
+def open_fuse(root: str, mount: str) -> fuse.FUSE:
+    return fuse.FUSE(CrytoOperations(root), mount, foreground=True, allow_other=False)
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('root')
+    parser.add_argument('mount')
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.DEBUG)
+    fuse_ = open_fuse(args.root, args.mount)
